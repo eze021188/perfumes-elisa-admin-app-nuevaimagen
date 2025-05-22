a5e7a5124cb351eb6cfbb05843305e12348b79b2
eze021188 <30888212+eze021188@users.noreply.github.com>
2025-05-22 16:49:27 -0600

    Update core dependencies and styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -38,8 +38,8 @@ module.exports = {
         sans: ['Inter var', 'system-ui', 'sans-serif'],
       },
       boxShadow: {
-        'elegant': '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
-        'card': '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
+        'card': '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
+        'card-hover': '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
         'dropdown': '0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)',
       }
     },
